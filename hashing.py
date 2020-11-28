c1010fcc172611e5d9ce1dea16647724f3551e63
NAVEED WINDOWS <imnaveed2003@gmail.com>
2020-11-29 00:05:49 +0400

    added

--- hashing.py
+++
@@ -1,8 +0,0 @@
-from cryptography.fernet import Fernet
-
-key = Fernet.generate_key()
-crypter = Fernet(key)
-pw = crypter.encrypt(b"PASS")
-decrypt=crypter.decrypt(pw)
-print(str(pw,'utf8'))
-print(str(decrypt,'utf8'))
